f15ec72ea787db47985a62d2051a61f6d63bb862
Bryan Bishop <bryan.bishop@pearson.com>
2016-08-25 21:44:54 -0500

    Fix for v3 of the API

--- commit_habitrpg.sh
+++ commit_habitrpg.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # quit and do nothing if the secrets file is missing
 [[ -f ~/.secrets/habitrpg.sh ]] || exit 0
 
 source ~/.secrets/habitrpg.sh
 
-\curl --retry 2 -m 5 -s -X POST -H "x-api-key: $HABITRPG_API_KEY" -H "x-api-user: $HABITRPG_API_USER" https://habitica.com/api/v3/tasks/$COMMIT_TASK_ID/up
+\curl -s -X POST -H "x-api-key: $HABITRPG_API_KEY" -H "x-api-user: $HABITRPG_API_USER" https://habitica.com/api/v3/tasks/$COMMIT_TASK_ID/score/up
